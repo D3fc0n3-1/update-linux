2d2cef655a45a6b862f2bea0eb3f7a54e3a6dd08
D3fc0n3.1 <93174251+KennethOldenburg@users.noreply.github.com>
2024-06-21 09:49:50 -0500

    Create setup.sh
    initial commit

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# URL of your GitHub repository containing the setup.sh script
+repo_url="https://github.com/KennethOldenburg/update-linux"
+
+# Name of the setup script
+script_name="upgrade"
+
+# Install the setup script
+install_script() {
+    # Clone the repository
+    git clone $repo_url
+
+    # Change into the cloned repository directory
+    cd update-linux
+
+    # Copy the setup script to /bin directory
+    sudo cp $script_name /bin/update
+
+    # Make the setup script executable
+    sudo chmod +x /bin/upgrade
+
+    # Clean up by removing the cloned repository directory
+    cd ..
+    rm -rf update-linux
+}
+
+# Main function
+main() {
+    install_script
+}
+
+# Execute the main function
+main
