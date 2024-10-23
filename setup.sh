0e8026f9f7b21f8e1e453584e3180a0be788ec6a
D3fc0n3-1 <dfcn3.1@gmail.com>
2024-10-22 20:00:33 -0500

    Update setup.sh
    add docker setup to initial run

--- setup.sh
+++ setup.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
-#
-# URL of your GitHub repository containing the setup.sh script
-#repo_url="https://github.com/KennethOldenburg/update-linux"
+# call docke script
+bash ./scripts/docker-engine.sh
 
-# Name of the setup script
-#script_name="upgrade"
+# Copy the setup script to /bin directory
+sudo cp update /bin/update
 
-# Install the setup script
-#install_script() {
-    # Clone the repository
-#    git clone $repo_url
+# Make the setup script executable
+sudo chmod +x /bin/update
 
-    # Change into the cloned repository directory
-#    cd update-linux
-
-    # Copy the setup script to /bin directory
-    sudo cp update /bin/update
-
-    # Make the setup script executable
-    sudo chmod +x /bin/update
-
-    # Clean up by removing the cloned repository directory
-    cd ..
-    rm -rf update-linux
-}
+# Clean up by removing the cloned repository directory
+cd ..
+rm -rf update-linux
 
 # Main function
 main() {
